e225a8a5cf2680e1d24dfefb6241427c5576c8c5
renatho <renatho@pop-os.localdomain>
2023-06-13 17:35:06 -0300

    tentando achar um erro

--- src/components/Div.tsx
+++ src/components/Div.tsx
@@ -3,9 +3,13 @@ type DivProp = {
 };
 
 function DivMensagem(senha:DivProp) {
+  const valido = 'senha e/ou login válido';
+  const invalido = 'senha e/ou login inválido';
   return (
     <div>
-      <p className={ senha.length >= 8 } />
+      <p className={ senha.length >= 8 ? valido : invalido } />
     </div>
   );
 }
+
+export default DivMensagem;
