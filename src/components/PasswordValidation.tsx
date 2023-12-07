b418c32ba417c0068ae1fc5feb81ee047a56de16
renatho <renatho@pop-os.localdomain>
2023-12-07 17:40:19 -0300

    adicionando h2

--- src/components/PasswordValidation.tsx
+++ src/components/PasswordValidation.tsx
@@ -11,33 +11,36 @@ function PasswordValidation({ password }: PasswordValidationProp) {
   const hasSymbols = /[!@#$%¨&*()|{}<>,]/;
 
   return (
-    <div>
-      <p className={ password.length >= 8 ? styles[available] : styles[unavailable] }>
-        Possuir 8 ou mais caracteres
-      </p>
-      <p
-        className={
+    <>
+      <h2>A senha deve possuir:</h2>
+      <div>
+        <p className={ password.length >= 8 ? styles[available] : styles[unavailable] }>
+          Possuir 8 ou mais caracteres
+        </p>
+        <p
+          className={
           password.length >= 1
           && password.length <= 16 ? styles[available] : styles[unavailable]
         }
-      >
-        Possuir até 16 caracteres
-      </p>
-      <p
-        className={ hasLettersAndNumbers.test(password)
-          ? styles[available]
-          : styles[unavailable] }
-      >
-        Possuir letras e números
-      </p>
-      <p
-        className={ hasSymbols.test(password)
-          ? styles[available]
-          : styles[unavailable] }
-      >
-        Possuir algum caractere especial
-      </p>
-    </div>
+        >
+          Possuir até 16 caracteres
+        </p>
+        <p
+          className={ hasLettersAndNumbers.test(password)
+            ? styles[available]
+            : styles[unavailable] }
+        >
+          Possuir letras e números
+        </p>
+        <p
+          className={ hasSymbols.test(password)
+            ? styles[available]
+            : styles[unavailable] }
+        >
+          Possuir algum caractere especial
+        </p>
+      </div>
+    </>
   );
 }
 
